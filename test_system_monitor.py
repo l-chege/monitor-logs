bb0b12411af47382f0a46981c982a62f0a8f01c6
lchege <laurachege9@gmail.com>
2024-05-10 14:38:53 +0300

    Wrote Unit tests for the script

---
+++ test_system_monitor.py
@@ -0,0 +1,10 @@
+import unittest
+from system_monitor import monitor_system
+
+class TestSystemMonitor(unittest.TestCase):
+    def test_monitor_system(self):
+        # Add test cases  to verify the functionality of the monitor_system function
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
