4eea96c3ccdd7c58bbb1e70ef86b5938bfcef0e5
lchege <laurachege9@gmail.com>
2024-05-10 14:35:43 +0300

    Created a configuration file

---
+++ config.py
@@ -0,0 +1,9 @@
+# Thresholds for alerts (in percentage)
+CPU_THRESHOLD = 80
+MEMORY_THRESHOLD = 80
+DISK_THRESHOLD = 80
+
+# Email settings
+SENDER_EMAIL = "your_email@gmail.com"
+RECIPIENT_EMAIL = "recipient_email@example.com"
+EMAIL_PASSWORD = "your_email_password"
